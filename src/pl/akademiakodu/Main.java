4adcdb6681966bea33a80e2e10531f4b5c1b95ac
Agata Gołaszewska <agata@gda.pl>
2018-08-22 12:31:00 +0200

    added WŁASNA próba GITa

--- src/pl/akademiakodu/Main.java
+++ src/pl/akademiakodu/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Pierwsza próba. ");
-        System.out.println("Dokładniej: to jest moja pierwsza WŁASNA próba !!");
+        System.out.println("Dokładniej: to jest moja pierwsza WŁASNA próba !! ");
+        System.out.println("Jeszcze dokładniej: pierwsza WŁASNA próba GITa !! ");
 
     }
 }
