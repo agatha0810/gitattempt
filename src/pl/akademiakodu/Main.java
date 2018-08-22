1acc9ac8dc7dc5c827ebee60a6c3b9501b41d987
Agata Gołaszewska <agata@gda.pl>
2018-08-22 12:24:42 +0200

    added sout WŁASNA

--- src/pl/akademiakodu/Main.java
+++ src/pl/akademiakodu/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Pierwsza próba. ");
+        System.out.println("Dokładniej: to jest moja pierwsza WŁASNA próba !!");
 
     }
 }
