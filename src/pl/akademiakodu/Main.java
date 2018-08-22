1c099ba9178717559e95573fe076f67bbe4ff0d6
Agata Gołaszewska <agata@gda.pl>
2018-08-22 12:46:34 +0200

    added sout WŁASNA

--- src/pl/akademiakodu/Main.java
+++ src/pl/akademiakodu/Main.java
@@ -2,11 +2,17 @@ package pl.akademiakodu;
 
 public class Main {
 
+    private  static double sum(double a, double b){
+        return a+b;
+    }
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Pierwsza próba. ");
         System.out.println("Dokładniej: to jest moja pierwsza WŁASNA próba !! ");
         System.out.println("Jeszcze dokładniej: pierwsza WŁASNA próba GITa !! ");
+
+        System.out.println("Suma liczb 2,5 i 3,5 wynosi: " + sum(2.5,3.5));
 
     }
 }
